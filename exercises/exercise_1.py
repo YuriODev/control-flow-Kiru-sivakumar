b04d5613e84b42e855311a18da0767f06a03c712
Kiru-sivakumar <157816684+Kiru-sivakumar@users.noreply.github.com>
2024-03-25 16:00:35 +0000

    Update exercise_1.py

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,10 @@
 # Your solution to Exercise 1
+a1 = int(input('Enter age of Alex: '))
+a2 = int(input("Please enter the age of Tarayana: "))
+if a2 >a1 :
+  print('Tatyanan is older.')
+elif a2<a1 :
+  print('Alex is older')
+else:
+  print("They are both the same age.")
 
